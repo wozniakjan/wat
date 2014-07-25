7e3ddeff79d24921a38c212053556522ec9efec1
Jan Wozniak <wozniak.jan@gmail.com>
2014-07-25 15:17:23 +0100

    valueOf

--- java/src/MainClass.java
+++ java/src/MainClass.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.io.Console;
+
 public class MainClass {
     public static void class_vs_primitive(){
+        Console console = System.console();
         /**
          * \brief. 
          * Difference between int and Integer is huge even though
@@ -19,21 +25,69 @@ public class MainClass {
         System.out.println("=================================");
         int a = 100, b = 100;
         System.out.println("int a = 100, b = 100;");
-        System.out.print(String.format("a == b; //%b", a==b)); 
-        
+        System.out.print("a == b;");
+        console.readLine();
+        System.out.print(a == b);
+        console.readLine();
+
         Integer a1 = 100, b1 = 100;
-        System.out.println("\n\nInteger a = 100, b = 100;");
-        System.out.print(String.format("a == b; //%b", a1==b1)); 
+        System.out.println("\nInteger a = 100, b = 100;");
+        System.out.print("a == b;");
+        console.readLine();
+        System.out.print(a1 == b1);
+        console.readLine();
         
         int a2 = 1000, b2 = 1000;
-        System.out.println("\n\nint a = 1000, b = 1000;");
-        System.out.print(String.format("a == b; //%b", a2==b2)); 
+        System.out.println("\nint a = 1000, b = 1000;");
+        System.out.print("a == b;");
+        console.readLine();
+        System.out.print(a2 == b2);
+        console.readLine();
         
         Integer a3 = 1000, b3 = 1000;
-        System.out.println("\n\nInteger a = 1000, b = 1000;");
-        System.out.print(String.format("a == b; //%b", a3==b3)); 
+        System.out.println("\nInteger a = 1000, b = 1000;");
+        System.out.print("a == b;");
+        console.readLine();
+        System.out.print(a3 == b3);
+        console.readLine();
 
-        System.out.println("\n\nWAT??");
+        System.out.println("\nWAT?");
+        console.readLine();
+
+        /**
+         * \brief.
+         * Overloading methods with int and Integer can be equally dangerous,
+         * for example if you try to remove an object from List, it is better
+         * to be extremely cautious
+         *
+         * \explanation
+         * - remove(Integer) removes object Integer
+         * - remove(int) removes object on the position int
+         **/
+        List<Integer> l = new ArrayList<Integer>(Arrays.asList(1,2,3));
+        int i = 1;
+        System.out.print("list: ");
+        System.out.println(l);
+        System.out.print("int i = 1;\nl.remove(i);");
+        l.remove(i);
+        console.readLine();
+        System.out.print("result: ");
+        System.out.print(l);
+        console.readLine();
+
+        List<Integer> l1 = new ArrayList<Integer>(Arrays.asList(1,2,3));
+        Integer i1 = 1;
+        System.out.print("\n\nlist: ");
+        System.out.println(l);
+        System.out.print("Integer i = 1;\nl.remove(i);");
+        l1.remove(i1);
+        console.readLine();
+        System.out.print("result: ");
+        System.out.print(l1);
+        console.readLine();
+        
+        System.out.println("\nWAT?");
+        console.readLine();
     }
     public static void main(String[] args) {
         class_vs_primitive();
--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,16 @@
+// overloaded + operator with array and object
 [] + [] //concatenation
 [] + {} //adds object to array
 {} + [] //not very clear why
 {} + {} //i could buy that
+
+// valueOf() method
+[1,2,3,4]+3 == "1,2,34"
+
+var three = { valueOf: function() { return 3; }} //object, valueOf() returns primitive
+three + three == 6;
+three - three == 0;
+var lenArray = [1,2,3,4];
+lenArray.valueOf = function() { return this.length; }
+three + lenArray == 7
+
