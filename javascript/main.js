0be445eb5ded2174dfad4c98bac28dd47d2da136
Jan Wozniak <wozniak.jan@gmail.com>
2014-07-30 07:13:47 +0100

    added sort and order

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,21 @@
 // overloaded + operator with array and object
-[] + [] //concatenation
-[] + {} //adds object to array
+[] + [] //concatenation with coercion
+[] + {} //kind of absorbation, probably?
 {} + [] //not very clear why
 {} + {} //i could buy that
 
+//well defined order.. not
+0 >= null;
+null >= 0;
+null == 0;
+
+//sorting array
+//Array.sort() forces all members toString(), though comparison between built-in types
+//is well defined
+[0, -1, -2].sort()
+
 // valueOf() method
-[1,2,3,4]+3 == "1,2,34"
+[1,2,3]+4 == "1,2,34"
 
 var three = { valueOf: function() { return 3; }} //object, valueOf() returns primitive
 three + three == 6;
